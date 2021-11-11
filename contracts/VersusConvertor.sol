cf384ba74609fc2c552d5529a9c60607d91c51b8
Ivan Solomichev <vanijatko.social@gmail.com>
2021-11-11 16:00:04 +0200

    Convertor implemented

---
+++ contracts/VersusConvertor.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "./uniswap/UniswapV2Pair.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract VersusConvertor {
+  address public pair_Versus_BUSD;
+  address public pair_BUSD_BNB;
+
+  /**
+   * @dev Constructor.
+   * @param _pair_Versus_BUSD Address of pair VERSUS - BUSD.
+   * @param _pair_BUSD_BNB Address of pair BUSD - BNB.
+   */
+  constructor(address _pair_Versus_BUSD, address _pair_BUSD_BNB) {
+    pair_Versus_BUSD = _pair_Versus_BUSD;
+    pair_BUSD_BNB = _pair_BUSD_BNB;
+  }
+
+  /**
+   * @dev Converts VERSUS to BNB.
+   * @param _amount VERSUS amount to be converted.
+   * @return BNB amount.
+   */
+  function convertVersusToBnb(uint256 _amount) external view returns(uint256) {
+    if (_amount == 0) {
+      return 0;
+    }
+
+    uint256 amount_BUSD = getTokenPrice_Versus_BUSD(pair_Versus_BUSD, _amount);
+    uint256 amount_bnb = getTokenPrice_BUSD_BNB(pair_BUSD_BNB, amount_BUSD);
+    return amount_bnb;
+  }
+
+  /**
+   * @dev Gets token price Versus -> BUSD.
+   * @param _pairAddress Pair address.
+   * @param _amount Amount to be converted from.
+   */
+  function getTokenPrice_Versus_BUSD(address _pairAddress, uint _amount) public view returns(uint) {
+    IUniswapV2Pair pair = IUniswapV2Pair(_pairAddress);
+    IERC20Metadata token1 = IERC20Metadata(pair.token1());
+    (uint Res0, uint Res1,) = pair.getReserves();
+
+    uint res0 = Res0 * (10**token1.decimals());
+    return((_amount * res0) / Res1); // return amount of token0 needed to buy token1
+   }
+
+   /**
+   * @dev Gets token price BUSD -> BNB.
+   * @param _pairAddress Pair address.
+   * @param _amount Amount to be converted from.
+   */
+  function getTokenPrice_BUSD_BNB(address _pairAddress, uint _amount) public view returns(uint) {
+    IUniswapV2Pair pair = IUniswapV2Pair(_pairAddress);
+    IERC20Metadata token1 = IERC20Metadata(pair.token1());
+    (uint Res0, uint Res1,) = pair.getReserves();
+
+    uint res0 = Res0 * (10**token1.decimals());
+    return((_amount * res0) / Res1); // return amount of token0 needed to buy token1
+   }
+}
---
+++ contracts/uniswap/UniswapV2Pair.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import './interfaces/IUniswapV2Pair.sol';
+
+contract UniswapV2Pair is IUniswapV2Pair {
+    address public token0;
+    address public token1;
+
+    uint112 private reserve0;           // uses single storage slot, accessible via getReserves
+    uint112 private reserve1;           // uses single storage slot, accessible via getReserves
+    uint32  private blockTimestampLast; // uses single storage slot, accessible via getReserves
+
+
+    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+        _reserve0 = reserve0;
+        _reserve1 = reserve1;
+        _blockTimestampLast = blockTimestampLast;
+    }
+}
